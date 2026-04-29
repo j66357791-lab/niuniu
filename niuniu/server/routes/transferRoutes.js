8a0831ea26e3dd741e45f11f64d9ba4ac2aedf7e
j66357791-lab <j66357791@gmail.com>
2026-04-29 22:17:00 +0800

    Delete niuniu/server directory‘

--- niuniu/server/routes/transferRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-import { transfer, getTransactions } from '../controllers/transferController.js'
-
-const router = Router()
-
-router.post('/transfer', transfer)
-router.get('/transactions', getTransactions)
-
-export default router
